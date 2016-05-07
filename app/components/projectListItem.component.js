f640b224db0e637c28568bc74631f684532ec1d2
Marios Fakiolas <m.fakiolas@hotmail.gr>
2016-05-08 01:32:58 +0300

    social buttons

--- app/components/projectListItem.component.js
+++ app/components/projectListItem.component.js
@@ -6,6 +6,25 @@
             bindings: {
                 project: '<'
             },
-            templateUrl: 'app/views/projectListItem.template.html'
+            templateUrl: 'app/views/projectListItem.template.html',
+            controller: projectListItemController
         });
+
+    function projectListItemController() {
+        var self = this;
+		self.projectLink = 'https://portfolio-url.com/#/projects/' + self.project.id + '/' + self.project.slug;
+        self.shareLink = function(network) {
+            if (network == 'facebook') {
+				return 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(self.projectLink);
+            }
+
+			if (network == 'twitter') {
+				return 'https://twitter.com/share?url=' + encodeURIComponent(self.projectLink);
+			}
+
+			if (network == 'linkedin') {
+				return 'https://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(self.projectLink);
+			}
+        }
+    }
 })();
